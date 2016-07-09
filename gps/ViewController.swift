f53c4b5c582d63a2329232ca3442e62394d0e7d1
Natalie Tay <natalie.tay@gmail.com>
2016-07-09 13:30:13 +0800

    Initial Commit

---
+++ gps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gps
+//
+//  Created by Natalie Tay on 9/7/16.
+//  Copyright © 2016 Natalie Tay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
